893b1c6c33b6f40c21d10d1fa3b894fa7d30304e
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-08 23:38:29 +0530

    relation with product defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :products, dependent: :destroy
   validates :email, uniqueness: true
   validates :email, format: { with: /@/ }
   validates :password_digest, presence: true
