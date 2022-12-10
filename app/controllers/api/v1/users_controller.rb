e832e0ad500291ab2523094a1aa02f0476a7ea19
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-10 14:24:08 +0530

    returning user json using serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,14 @@ class Api::V1::UsersController < ApplicationController
   before_action :check_owner, only: [:update, :destroy]
 
   def show
-    render json: @user
+    options = { include: [:products] }
+    render json: UserSerializer.new(@user, options).serializable_hash.to_json
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash.to_json, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -17,7 +18,7 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: UserSerializer.new(@user).serializable_hash.to_json, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
