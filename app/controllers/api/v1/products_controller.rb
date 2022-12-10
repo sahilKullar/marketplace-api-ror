128233b9e4456f8eb9ab4aefe69c8cf6ee3f110f
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-10 14:23:11 +0530

    returning json using serializer

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,17 +4,19 @@ class Api::V1::ProductsController < ApplicationController
   before_action :check_owner, only: %i[update destroy]
 
   def index
-    render json: Product.all
+    @products = Product.search(params)
+    render json: ProductSerializer.new(@products).serializable_hash.to_json
   end
 
   def show
-    render json: @product
+    options = { include: [:user] }
+    render json: ProductSerializer.new(@product, options).serializable_hash.to_json
   end
 
   def create
     @product = current_user.products.build(product_params)
     if @product.save
-      render json: @product, status: :created
+      render json: ProductSerializer.new(@product).serializable_hash.to_json, status: :created
     else
       render json: { error: @product.errors }, status: :unprocessable_entity
     end
@@ -22,7 +24,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      render json: @product, status: :ok
+      render json: ProductSerializer.new(@product).serializable_hash.to_json, status: :ok
     else
       render json: { error: @product.errors }, status: :unprocessable_entity
     end
