0a599283a8797a64366eed4e6cb4d6125389ac59
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-10 19:43:31 +0530

    add quantity to product and placement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_090929) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_121035) do
   create_table "orders", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.decimal "total", precision: 10
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_090929) do
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
     t.index ["order_id"], name: "index_placements_on_order_id"
     t.index ["product_id"], name: "index_placements_on_product_id"
   end
@@ -35,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_090929) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
