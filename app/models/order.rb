325b0b7b2963877bbb264f74dda8178168dd06cd
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-10 16:58:24 +0530

    set total method defined in order to calculate the product total price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
-  validates :total, numericality: { greater_than_or_equal_to: 0 }, presence: true
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
+  # validates :total, numericality: { greater_than_or_equal_to: 0 }, presence: true
+  before_validation :set_total!
+
+  def set_total!
+    # self.total = products.map(&:price).sum
+    self.total = products.sum(&:price)
+  end
 end
