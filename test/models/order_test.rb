dd09b970e539da9a8de11fc2180fe6b950809ca3
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-10 16:58:58 +0530

    model test coded

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 class OrderTest < ActiveSupport::TestCase
-  test "should have a positive total" do
-    order = orders(:one)
-    order.total = -1
-    assert_not order.valid?
+  setup do
+    @order = orders(:one)
+    @product_one = products(:one)
+    @product_two = products(:two)
+  end
+
+  # test "should have a positive total" do
+  #   @order.total = -1
+  #   assert_not @order.valid?
+  # end
+
+  test "should set total" do
+    order = Order.new user_id: @order.user_id
+    order.products << products(:one)
+    order.products << products(:two)
+    order.save
+    assert_equal (@product_one.price + @product_two.price), order.total
   end
 end
